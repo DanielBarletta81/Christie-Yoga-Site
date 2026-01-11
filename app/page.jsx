7d8d36377724ea8f03da8331d2efc7ecaf7103f1
DanielBarletta81 <dbarcelona1481@gmail.com>
2026-01-10 21:57:30 -0500

    fix css and page.jsx import

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import HeroSection from '../components/HeroSection';
 import ImageCarousel from '../components/ImageCarousel';
 import SplitSection from '../components/SplitSection';
